5208ac728453059f343197af6d5bb7d439b0ce89
Alexander <eisven@yandex.ru>
2024-04-01 11:07:23 +0300

    notifications POC

--- src/screens/Home/ActionBar/ActionBar.js
+++ src/screens/Home/ActionBar/ActionBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { arrayOf, string, shape, number, func } from 'prop-types';
 import { View } from 'react-native';
+import notifee, { TriggerType } from '@notifee/react-native';
 import { useTranslation } from 'react-i18next';
 import colors from '~styles/colors';
 import Button from '~UI/Button';
@@ -16,6 +17,38 @@ const ActionBar = ({ filterParams, totalItems, showFilters, activeFiltersCount }
   const categoriesLength = filterParams?.categoryPaths?.length;
   const isActiveFilter = categoriesLength > 0;
 
+  const displayNotification = async () => {
+    const date = new Date(Date.now());
+    date.setMinutes(30);
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    const trigger = {
+      type: TriggerType.TIMESTAMP,
+      timestamp: date.getTime(),
+    };
+
+    await notifee.displayNotification({
+      id: '123',
+      title: 'Meeting with Jane',
+      body: 'Today at 11:20am',
+      android: {
+        channelId,
+        smallIcon: 'ic_stat_name',
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  };
+
+  notifee.getTriggerNotificationIds().then((ids) => console.log('All trigger notifications: ', ids));
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.container}>
@@ -29,6 +62,7 @@ const ActionBar = ({ filterParams, totalItems, showFilters, activeFiltersCount }
             onPress={showFilters}
             title={isActiveFilter ? t('categoriesCount', { count: activeFiltersCount }) : t('categoriesTitle')}
           />
+          <Button title='Display Notification' onPress={() => displayNotification()} />
         </View>
         <TotalCount count={totalItems} />
       </View>
