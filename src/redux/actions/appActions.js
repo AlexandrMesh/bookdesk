71a1b3c4ef3432be1fd7f39598e9eddfb6d00296
Alexander Meshok <alexandermeshok@coherentsolutions.com>
2024-07-19 12:50:12 +0300

    fix build gradle

--- src/redux/actions/appActions.js
+++ src/redux/actions/appActions.js
@@ -7,6 +7,7 @@ const PREFIX = 'APP';
 export const START_LOADING_APP_INFO = `${PREFIX}/START_LOADING_APP_INFO`;
 export const APP_INFO_LOADED = `${PREFIX}/APP_INFO_LOADED`;
 export const LOADING_APP_INFO_FAILED = `${PREFIX}/LOADING_APP_INFO_FAILED`;
+
 export const CLEAR_APP_INFO = `${PREFIX}/CLEAR_APP_INFO`;
 
 export const startLoadingAppInfo = {
